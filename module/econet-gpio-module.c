6b39f58ee02ae4e8d3deb1aadc47e53426befd0a
Robert Franklin <rcf@mince.net>
2024-04-07 19:01:43 +0100

    limited clock in kernel module

--- module/econet-gpio-module.c
+++ module/econet-gpio-module.c
@@ -2339,12 +2339,12 @@ void econet_set_pwm(uint8_t period, uint8_t mark)
 	// Set range & data - constrain our parameters to 63/4 us period (15.something us), and
 	// 15/4 us mark (3.something us)
 
-	writel(period & 0x3f, (GPIO_PWM + PWM_RNG1));
+	writel(period & 0x1f, (GPIO_PWM + PWM_RNG1));
 	writel(mark & 0x0f, (GPIO_PWM + PWM_DAT1));
 
 	// Enable the PWM
 
-	if ((period & 0x3f) > (mark & 0x0f)) // If resulting mark is < resulting period, don't bother enabling the PWM because it'll be nonsense
+	if ((period & 0x1f) > (mark & 0x0f)) // If resulting mark is < resulting period, don't bother enabling the PWM because it'll be nonsense
 	{
 		writel(	(readl(GPIO_PWM + PWM_CTL) & ~(0xff)) | (PWM_CTL_MSEN1 | PWM_CTL_PWEN1),
 			(GPIO_PWM + PWM_CTL)	);
